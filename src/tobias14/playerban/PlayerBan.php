d0f590477501f266b1b8015b0dd8b5af498abc5c
AbruptYT <tobiaskirchmaier44@gmail.com>
2021-03-31 10:01:38 +0200

    Added messages configuration

--- src/tobias14/playerban/PlayerBan.php
+++ src/tobias14/playerban/PlayerBan.php
@@ -2,30 +2,41 @@
 
 namespace tobias14\playerban;
 
+use pocketmine\lang\BaseLang;
 use pocketmine\plugin\PluginBase;
 use tobias14\playerban\database\Database;
 
 class PlayerBan extends PluginBase {
 
     private static $instance;
+    private $baseLang;
 
     public static function getInstance() : self {
         return self::$instance;
     }
 
-    public function onEnable() {
+    public function getLang() : BaseLang {
+        return $this->baseLang;
+    }
+
+    public function onLoad() {
         self::$instance = $this;
 
         $this->saveDefaultConfig();
 
+        $this->saveResource("eng.ini");
+        $this->baseLang = new BaseLang("eng", $this->getDataFolder());
+
         if(false === Database::checkConnection()) {
-            $this->getLogger()->error("Could not connect to database!");
+            $this->getLogger()->error($this->getLang()->translateString("db.connection.failed"));
             $this->getServer()->getPluginManager()->disablePlugin($this);
             return;
         }
 
         Database::init();
+    }
 
+    public function onEnable() {
         $command_map = $this->getServer()->getCommandMap();
         $commands = ["ban", "unban", "pardon", "ban-ip", "unban-ip"];
         foreach ($commands as $cmd) {
